e3b0b1326ba8c948da1324e2558598343f9c00eb
Abhijeet Kumar <76590629+Abhijeet0803@users.noreply.github.com>
2023-02-02 15:16:24 +0530

    Chapter 10

---
+++ Chapter10/src/app.js
@@ -0,0 +1,74 @@
+ import React from "react";
+ import ReactDOM, {createElement} from 'react-dom/client';
+ import Header from './components/Header';
+ import Body from './components/Body';
+ import Footer from "./components/Footer";
+ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+ import About from "./components/About";
+ import Error from "./components/Error";
+import Contact from "./components/contact";
+import RestaurantMenu from "./components/RestaurantMenu";
+
+ {
+   /* PLANNING
+      -Header
+         -Logo
+         -Nav Items(Right Side)
+         -Cart
+      -Body
+         -Search Bar
+         -Restaurant Cards
+            - Image
+            - Name
+            - Cuisines
+            - Rating
+      -Footer
+         -Links
+         -Copyrights
+
+   */   
+ }
+ 
+  
+
+    const AppLayout = () =>{
+      return(<>
+         <Header />
+         <Outlet />
+         <Footer /> 
+      </>);
+    }
+
+    const appRouter = createBrowserRouter([
+      {
+         path:"/",
+         element: <AppLayout />,
+         errorElement: <Error />,
+         children:[
+            {
+               path:"/",
+               element: <Body />
+            },
+            {
+               path:"/about",
+               element: <About />
+            },
+            {
+               path:"/contact",
+               element: <Contact />
+            },
+            {
+               path:"/restaurant/:id",
+               element: <RestaurantMenu />
+            }
+         ]
+      },
+      
+    ]);
+
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));  
+    root.render(<RouterProvider router={appRouter} />);
+    
+
+    
---
+++ Chapter10/src/components/Header.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/food_villa.png"
+
+ const Title = () => (<Link to={"/"}>
+    <img className="h-28 p-2" alt="logo" 
+    src={Logo} />
+    </Link>);
+
+const Header = () =>{
+  
+  const [islogin, setIsLogin] = useState(false);
+
+  // useEffect(()=>{
+  //   console.log("useEffect");
+  // });
+ // console.log(useState());
+
+  return(<div className="flex justify-between px-4 bg-pink-100 shadow-md">
+    <Title />
+    <div>
+      <ul className="flex justify-between py-10">
+         <li className="px-2"><Link to={"/"}>Home</Link></li>
+         <li className="px-2"><Link to={"/about"}>About</Link></li>
+         <li className="px-2"><Link to={"/contact"}>Contact</Link></li>
+         <li className="px-2">Cart</li>
+         <li className="px-2">{islogin?<button onClick={()=>setIsLogin(false)}>Logout</button>:<button onClick={()=>setIsLogin(true)}>Login</button>}</li>
+      </ul>
+    </div>
+    </div>)};
+
+export default Header;
